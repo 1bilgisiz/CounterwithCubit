d533a7114175ea05c230b1651b3b7a0e144d1eb6
1bilgisiz <bilgicsaniye66@gmail.com>
2024-12-17 11:43:10 +0300

    desing added

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,65 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Counter App",
+        theme: ThemeData(primaryColor: Colors.blue),
+        home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "Counter App",
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("You have pushed the button this any times"),
+              Text(
+                "0",
+                style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {},
+              tooltip: "Tıkla",
+              backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(44)),
+              child: const Icon(Icons.add),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              tooltip: "Tıkla",
+              backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(44)),
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ));
   }
 }
